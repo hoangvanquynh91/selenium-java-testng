51eb45aeed34f65e7c7834d3946aac415853beed
hoangvanquynh91 <hoangvanquynh1991@gmail.com>
2022-10-10 14:20:19 +0700

    demo topic 2

--- src/webdriver/Topic_02_Selenium_Locator.java
+++ src/webdriver/Topic_02_Selenium_Locator.java
@@ -2,6 +2,7 @@ package webdriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -16,44 +17,58 @@ public class Topic_02_Selenium_Locator {
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/");
+		driver.get("https://demo.nopcommerce.com/register");
 	}
 
 	@Test
 	public void TC_01_ID() {
+		driver.findElement(By.id("FirstName")).sendKeys("Automation");
 		
 	}
 
 	@Test
 	public void TC_02_Class() {
+		// Mở màn hình search
+		driver.get("https://demo.nopcommerce.com/search");
+		driver.findElement(By.className("search-text")).sendKeys("DELL");
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
 		
 	}
 
 	@Test
 	public void TC_03_Name() {
-		
+		//Click vào addvanced search checkbox
+		driver.findElement(By.name("advs")).click();
 	}
 	
 	@Test
 	public void TC_04_TagName() {
+		// tìm số lượng thẻ input trên màn hình hiện tại
+		
+		System.out.print(driver.findElements(By.tagName("input")).size());
 		
 	}
 	
 	@Test
 	public void TC_05_LinkText() {
+		//Click vao duong link
+		driver.findElement(By.linkText("Addresses")).click();
 		
 	}
 	
 	@Test
 	public void TC_06_PartialLinkText() {
 		
+		driver.findElement(By.partialLinkText("Apply for vendor")).click();
+		
 	}
 	
 	@Test
 	public void TC_07_CSS() {
-		
+		//Mo trang register
+		driver.get("https://demo.nopcommerce.com/register");
 	}
 	
 	@Test
---
+++ src/webdriver/demo.java
@@ -0,0 +1,12 @@
+package webdriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = a;
+		a = 7;
+		System.out.print(b);
+	}
+
+}
