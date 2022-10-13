19b4471bdf28d3b9f613c3cc3efe06a726901335
hoangquynh91 <hoangvanquynh1991@gmail.com>
2022-10-14 06:56:20 +0700

    topic3

--- src/webdriver/Topic_03_XPath_Part1.java
+++ src/webdriver/Topic_03_XPath_Part1.java
@@ -3,15 +3,13 @@ package webdriver;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class demo {
+public class Topic_03_XPath_Part1 {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 
@@ -19,18 +17,26 @@ public class demo {
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://tiki.vn/");
+		//driver.get("https://www.facebook.com/");
 	}
 
 	@Test
 	public void TC_01() {
-		driver.findElement(By.xpath("//input[@data-view-id=\"main_search_form_input\"]")).sendKeys("xe may");
+		driver.get("http://live.techpanda.org/index.php/customer/account/login/");
+		
+		//Click vaof my account link duoi footer
+		driver.findElement(By.xpath("//div[@class = 'footer']//a[@title='My Account']")).click();
 	}
 
 	@Test
 	public void TC_02() {
+		
+		driver.get("https://automationfc.github.io/basic-form/");
+		
+		
+		System.out.println(driver.findElement(By.xpath("//h5[@id = 'nested']")).getText());
 		
 	}
 
